5e8d8b1fa040c53cae8acf78ffed27f8d5da1cad
ingavaisanen <128291687+ingavaisanen@users.noreply.github.com>
2023-04-11 17:38:09 +0300

    Update GroceryViewHolder.java

--- GroceryViewHolder.java
+++ GroceryViewHolder.java
@@ -1,38 +1,19 @@
 package com.example.grocerylist;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+public class GroceryViewHolder extends RecyclerView.ViewHolder {
 
-public class GroceryListAdapter extends RecyclerView.Adapter<GroceryViewHolder> {
+    TextView GroceryName, GroceryDefinition;
 
-    private Context context;
-    private ArrayList<Grocery> lGroceries = new ArrayList<>();
+    public GroceryViewHolder(@NonNull View itemView) {
+        super(itemView);
 
-    public GroceryListAdapter(Context context, ArrayList<Grocery> lGroceries) {
-        this.context = context;
-        this.lGroceries = lGroceries;
-    }
-
-    @NonNull
-    @Override
-    public GroceryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new GroceryViewHolder(LayoutInflater.from(context).inflate(R.layout.grocery_view, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull GroceryViewHolder holder, int position) {
-        holder.GroceryName.setText(lGroceries.get(position).getGroceries());
-        holder.GroceryDefinition.setText("Muista: " + lGroceries.get(position).getDefinition());
-    }
-
-    @Override
-    public int getItemCount() {
-        return lGroceries.size();
+        GroceryName = itemView.findViewById(R.id.tvgrocery);
+        GroceryDefinition = itemView.findViewById(R.id.tvDefinition);
     }
 }
